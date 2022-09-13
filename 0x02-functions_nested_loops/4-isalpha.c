10cd8a311921dc7cd9ddb73db2352c198acb2db2
Getasew Wondim <87017416+getasewk@users.noreply.github.com>
2022-09-13 21:03:01 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,23 +1,22 @@
 #include "main.h"
 
 /**
- * _isalpha - check the code for Holberton School students.
- *
- * @c: is c an integer argument
+ * main - check the code.
  *
  * Return: Always 0.
  */
-
-int _isalpha(int c)
+int main(void)
 {
+    int r;
 
-	if (c >= 97 && c <= 122)
-	{
-		return (1);
-	}
-	else if (c >= 65 && c <= 90)
-	{
-		return (1);
-	}
-	return (0);
+    r = _isalpha('H');
+    _putchar(r + '0');
+    r = _isalpha('o');
+    _putchar(r + '0');
+    r = _isalpha(108);
+    _putchar(r + '0');
+    r = _isalpha(';');
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
 }
