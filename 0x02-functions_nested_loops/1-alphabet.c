92565a1c769b739e5e6648885c458c5aa58f3a3a
Getasew Wondim <87017416+getasewk@users.noreply.github.com>
2022-09-13 21:26:20 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,19 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - check the code.
  *
  * Return: Always 0.
  */
-int main(void)
+
+void print_alphabet(void)
 {
-    print_alphabet();
-    return (0);
+	char alp = 'a';
+
+	while (alp <= 'z')
+	{
+		_putchar (alp);
+		alp++;
+	}
+	_putchar ('\n');
 }
