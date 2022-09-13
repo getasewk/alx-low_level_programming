ea596388952633d94b2202ab457cfc6f6e5265da
Getasew Wondim <87017416+getasewk@users.noreply.github.com>
2022-09-13 21:05:04 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,12 @@
 #include "main.h"
 
 /**
- * print_alphabet - check the code for Holberton School students.
+ * main - check the code
  *
  * Return: Always 0.
  */
-
-void print_alphabet(void)
+int main(void)
 {
-	char alp = 'a';
-
-	while (alp <= 'z')
-	{
-		_putchar (alp);
-		alp++;
-
-	}
-	_putchar ('\n');
-
+    print_alphabet();
+    return (0);
 }
