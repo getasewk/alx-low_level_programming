0d3f96d5124f3bf199a1af5a1d0e4a7818a24736
Getasew Wondim <87017416+getasewk@users.noreply.github.com>
2022-09-13 22:15:06 +0300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,18 +1,21 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * print_last_digit - prints last digit of integer input
+ * @n: integer to modify
  *
- * Return: Always 0.
+ * Return: l
  */
-int main(void)
-{
-	int r;
 
-	print_last_digit(98);
-	print_last_digit(0);
-	r = print_last_digit(-1024);
-	_putchar('0' + r);
-	_putchar('\n');
-	return (0);
+
+int print_last_digit(int n)
+{
+	int l, p;
+
+	l = n % 10;
+	if (l <  0)
+		l = -l;
+	p = '0' + l;
+	_putchar(p);
+	return (l);
 }
