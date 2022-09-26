1a82f1fd6ad40207025f835ef035ba2f4bf84542
Getasew Wondim <87017416+getasewk@users.noreply.github.com>
2022-09-26 19:05:50 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,21 +1,21 @@
+#include "main.h"
+
 /**
- * _memset - fills n bytes of memory
+ * *_memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
  *
- * @s: pointer to start of memory area
- * @b: constant byte to fill with
- * @n: num bytes to fill in
- *
- * Return: pointer to beginning of memory area s
+ * Return: pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
-	char *start = s;
+	unsigned int i;
 
-	while (i < n)
+	for (i = 0; i < n; i++)
 	{
-		*s++ = b;
-		i++;
+		s[i] = b;
 	}
-	return (start);
+
+	return (s);
 }
